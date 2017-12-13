b6d96514681aed4293e466c9c7dc41756d2d091b
Jim <jim95.teran@gmail.com>
2017-12-13 19:49:12 -0400

    cambios de obtener salario de auxiliar

--- models/Alumno.cpp
+++ models/Alumno.cpp
@@ -7,3 +7,15 @@ Alumno::Alumno( string n, string a, char s, int codsis) : Persona(n, a, s) {
 int Alumno::getCodigoSis() {
   return codigoSis;
 }
+
+void Alumno::agregarGrupo(Grupo grupo) {
+  gruposAuxiliatura.push_back(grupo);
+}
+
+float Alumno::salario() {
+  float salario = 0.0;
+  if (gruposAuxiliatura.size() > 0) {
+    salario = gruposAuxiliatura.size() * 40.0;
+  }
+  return salario;
+}
--- models/Alumno.hpp
+++ models/Alumno.hpp
@@ -1,13 +1,19 @@
 #ifndef ALUMNO_HPP
 #define ALUMNO_HPP
 #include "Persona.hpp"
+#include "Grupo.hpp"
+
+using std::vector;
 
 class Alumno : public Persona {
   private:
     int codigoSis;
+    vector<Grupo> gruposAuxiliatura;
   public:
     Alumno(string n, string a, char s, int c = 0);
     int getCodigoSis();
+    void agregarGrupo(Grupo grupo);
+    float salario();
 };
 
 #endif
