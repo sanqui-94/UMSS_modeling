f4e93944b9efccf3136df542ff7910c22e374793
rod8 <rivera.rodrigo08@gmail.com>
2017-12-13 16:39:48 -0400

    Se agrego la funcionalidad para que sepa comparar los horarios con otro grupo, devolviendo un vector con los horarios que tengan choque

--- models/Grupo.hpp
+++ models/Grupo.hpp
@@ -1,15 +1,20 @@
 #ifndef GRRUPO_HPP
 #define GRRUPO_HPP 1
 
-#include<string>
+#include <string>
+#include <vector>
+#include "Horario.hpp"
 
 using namespace std;
 
 class Grupo {
 	private:
 		string nombre;
+		vector<Horario> horarios;
 	public:
 		Grupo(string n = "");
+		void agregarHorario(Horario horario);
+		vector<Horario> choqueDeHorarios(Grupo &otro);
 		string getNombre();
 };
 
