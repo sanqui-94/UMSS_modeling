04b23ec6994fa73e8f3813d21d879b126bb8e28e
Antonio Alurralde Sanchez <antonio@publiaciertos.net>
2017-12-13 20:25:44 -0400

    Test Malla::toposort y varias correcciones el metodo

--- models/Malla.hpp
+++ models/Malla.hpp
@@ -1,15 +1,16 @@
 #ifndef MALLA_HPP
 #define MALLA_HPP 1
 
-#include "Materia.hpp"
+#include "Materia.cpp"
 #include <set>
 #include <map>
 
 class Malla {
 	private:
-		map<Materia, pair <string, vector<Materia> > > malla;
-		void visit(Materia *u, set<Materia> *visitados, vector<Materia> *res);
+		map<Materia, pair<string, vector<Materia>>> malla;
+		void visit(const Materia &u, set<Materia> &visitados, vector<Materia> &res);
 	public:
+		Malla(map<Materia, pair <string, vector<Materia> > > m);
 		vector<Materia> toposort();
 		bool tieneChoques(string nivel);
 };
