3f25376192ff7c9bffec47dd98a1405944c04ee3
Spitpat <san.quit@hotmail.com>
2017-12-13 21:35:50 -0400

    hoistoria cargo disenhada, falta la implementacion del metodo plantilla por gestion

--- models/HistoriaCargo.cpp
+++ models/HistoriaCargo.cpp
@@ -3,4 +3,22 @@
 HistoriaCargo::HistoriaCargo() {
 }
 
-//
+void HistoriaCargo::agregarPersona(Persona persona) {
+	/*
+	Cargo cargoVacio;
+	pair <Cargo, string> p = make_pair(cargoVacio, ""); */4
+	vector< pair <Cargo, string> > v; 
+	registro[persona] = v;
+}
+
+void HistoriaCargo::actualizarPersona(Persona persona, Cargo cargo, string gestion) {
+	vector < pair <Cargo, string> > vectorAuxiliar = registro[persona]
+	pair <Cargo, string> par = make_pair(cargo, gestion);
+	vectorAuxiliar.push_back(par);
+	registro[persona] = vectorAuxiliar;
+}
+
+vector <Persona, Cargo> HistoriaCargo::plantillaPorGestion(string gestion) {
+	vector <Persona, Cargo> respuesta;
+	return respuesta
+}
--- models/HistoriaCargo.hpp
+++ models/HistoriaCargo.hpp
@@ -4,14 +4,21 @@
 #include <map>
 #include <vector>
 #include <utility>
+#include <string>
 #include "Persona.hpp"
 #include "Cargo.hpp"
+
+using std::map;
+using std::vector;
+using std::pair;
 
 class HistoriaCargo {
  private:
  	map <Persona, vector <pair <Cargo, string> > > registro;
  public:
  	HistoriaCargo();
- 	//metodo para devolver la historia de cargos por gestion
+ 	void agregarPersona(Persona persona);
+ 	void actualizarPersona(Persona persona, Cargo cargo, string gestion);
+ 	vector <Persona, Cargo> plantillaPorGestion(string gestion);
  }; 
 #endif
